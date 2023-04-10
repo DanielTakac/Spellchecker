37315cadfee3d95b02b02a9ffce6b7cb44cca437
DanielTakac <danieltakac@protonmail.com>
2023-04-10 13:01:43 +0200

    Initial

---
+++ Spellchecker/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Spellchecker {
+    
+    class Program {
+
+        static void Main(string[] args) {
+
+            string[] dictionary = File.ReadAllLines(@"D:\100k.txt");    
+
+            var checker = new Spellchecker(dictionary);
+
+            string word = "city";
+
+            string sentence = "This ise a sentence with charcters, like 123 amd %$#!";
+
+            checker.CheckSentence(sentence, true);
+
+            Console.ReadKey();
+
+        }
+    
+    }
+
+}
---
+++ Spellchecker/Spellchecker.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Spellchecker {
+
+    class Spellchecker {
+
+        private string[] dictionary;
+
+        public Spellchecker() {
+
+            this.dictionary = new string[0];
+
+        }
+
+        public Spellchecker(string[] dictionary) {
+
+            this.dictionary = dictionary;
+
+        }
+
+        public void SetDictionary(string[] dictionary) {
+
+            this.dictionary = dictionary;
+
+        }
+
+        public bool CheckWord(string word) {
+
+            string cleanedWord = Regex.Replace(word, "[^a-zA-Z]+", "").ToLower();
+
+            if (cleanedWord.Length == 0) return true;
+
+            for (int i = 0; i < dictionary.Length; i++) {
+
+                if (dictionary[i] == cleanedWord) {
+
+                    return true;
+
+                }
+
+            }
+
+            return false;
+
+        }
+
+        public bool CheckSentence(string sentence, bool printResult = false) {
+
+            string[] words = sentence.Split(new char[] { });
+
+            List<string> badWords = new List<string> { };
+
+            for (int i = 0; i < words.Length; i++) {
+
+                if (!CheckWord(words[i])) {
+
+                    badWords.Add(words[i]);
+
+                }
+
+            }
+
+            if (printResult) PrintCheckedSentence(words, badWords);
+
+            if (badWords.Count > 0) return false;
+
+            return true;
+
+        }
+
+        public void PrintCheckedSentence(string[] words, List<string> badWords) {
+
+            for (int i = 0; i < words.Length; i++) {
+
+                if (badWords.Contains(words[i])) {
+
+                    Console.ForegroundColor = ConsoleColor.Red;
+
+                } else {
+
+                    Console.ForegroundColor = ConsoleColor.Green;
+
+                }
+
+                Console.Write(words[i] + " ");
+
+            }
+
+            Console.ResetColor();
+
+            Console.WriteLine();
+
+        }
+
+    }
+
+}
